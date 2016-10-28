c489554cd3ae4216c52298c8534ba628df71fc16
archana <archana.joshi@sjsu.edu>
2016-10-28 12:26:27 -0700

    new push.

---
+++ jenkins/src/jenkins/addnumbers.java
@@ -0,0 +1,8 @@
+package jenkins;
+
+public class addnumbers {
+ 
+	public int add(int num1 , int num2) {
+		 return (num1+num2);
+	}
+}
---
+++ jenkins/src/jenkins/multi.java
@@ -0,0 +1,7 @@
+package jenkins;
+
+public class multi {
+	public int prod(int num1 , int num2) {
+		 return (num1*num2);
+}
+}
