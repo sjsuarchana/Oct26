95b19b1db5452ba0d6dda1682130b832e65e3c9a
archana <archana.joshi@sjsu.edu>
2016-10-28 12:22:40 -0700

    df

---
+++ jenkins/src/jenkins/Jenkinmain.java
@@ -0,0 +1,22 @@
+package jenkins;
+
+import java.util.Scanner;
+
+public class Jenkinmain {
+
+	public static void main(String[] args) {
+	
+		Scanner getinput = new Scanner(System.in);
+		//System.out.println("TESTING ");
+		int num1 = getinput.nextInt();
+		int num2 = getinput.nextInt();
+		addnumbers change1 = new addnumbers();
+		int sum = change1.add(num1, num2);
+		System.out.println("sum is "+sum);
+		multi change2 = new multi();
+		int product = change2.prod(num1,num2);
+		System.out.println(product);
+		getinput.close();
+	}
+
+}
